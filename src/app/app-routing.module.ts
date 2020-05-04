d8ff5b85eebb24734143cd16ccdefbf6b5744263
Weinman <danyerra@gmail.com>
2020-05-04 10:49:35 +0200

    type: feat
    Added Login Component. Created the router list for the app.
    
    see also: #b8bc0af

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BedBaseComponent } from './product/bed-base/bed-base.component';
+import { BedBaseListComponent } from './product/bed-base-list/bed-base-list.component';
+import { MattressComponent } from './product/mattress/mattress.component';
+import { MattressListComponent } from './product/mattress-list/mattress-list.component';
+import { DashboardComponent } from './web/dashboard/dashboard.component';
+import { PasswordComponent } from './user/password/password.component';
+import { LoginComponent } from './user/login/login.component';
 
-
-const routes: Routes = [];
+/**
+* @title: Route List
+* @dev: Add guards, stop using retrieve route.
+* @description: Application routing module
+*/
+const routes: Routes = [
+	{ path:'', component: DashboardComponent },
+	{ path:'colchones', component: MattressListComponent },
+	{ path:'colchones/:id', component: MattressComponent },
+	{ path:'somieres', component: BedBaseListComponent },
+	{ path:'somieres/:id', component: BedBaseComponent },
+	{ path:'login', component: LoginComponent },
+	{ path:'retrieve', component: PasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
